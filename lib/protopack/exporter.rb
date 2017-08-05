83e44f44da0ca098bdc5b9d2cd0145608b2de39c
conanite <conan@conandalton.net>
2017-08-05 16:13:01 +0200

    exporter: avoid missing config keys

--- lib/protopack/exporter.rb
+++ lib/protopack/exporter.rb
@@ -1,16 +1,17 @@
 module Protopack
   class Exporter
-    def maybe_name_methods   ; %i{ export_name full_name presentation_name name }                          ; end
-    def name_method obj      ; maybe_name_methods.detect { |m| obj.respond_to?(m) && obj.send(m) }         ; end
-    def array_assoc list     ; list.map { |item| to_attributes item }                                      ; end
-    def clean_yaml hsh       ; StyledYAML.dump(clean_attrs hsh)                                            ; end
-    def no_name? obj_id      ; raise "no name for object: #{obj_id.inspect}" if obj_id.blank?              ; end
-    def export_config    obj ; obj.protopack_export_config || { fields: [], associations: [] }             ; end
-    def to_attributes obj    ; obj.slice(*export_config(obj)[:fields]).merge build_association_table obj   ; end
-    def to_yaml obj, meta={} ; clean_yaml to_package obj, meta                                             ; end
-    def remove_blanks attrs  ; attrs.recursively_remove_by_value! { |v| (v != false) && v.blank? }         ; end
-    def styled_literal str   ; str.is_a?(String) ? StyledYAML.literal(str) : str                           ; end
-    def clean_attrs attrs    ; remove_blanks(attrs).recursively_replace_values! { |k,v| styled_literal v } ; end
+    def maybe_name_methods    ; %i{ export_name full_name presentation_name name }                          ; end
+    def name_method obj       ; maybe_name_methods.detect { |m| obj.respond_to?(m) && obj.send(m) }         ; end
+    def array_assoc list      ; list.map { |item| to_attributes item }                                      ; end
+    def clean_yaml hsh        ; StyledYAML.dump(clean_attrs hsh)                                            ; end
+    def no_name? obj_id       ; raise "no name for object: #{obj_id.inspect}" if obj_id.blank?              ; end
+    def default_export_config ; { fields: [], associations: [] }                                            ; end
+    def export_config     obj ; default_export_config.merge obj.protopack_export_config                     ; end
+    def to_attributes     obj ; obj.slice(*export_config(obj)[:fields]).merge build_association_table obj   ; end
+    def to_yaml  obj, meta={} ; clean_yaml to_package obj, meta                                             ; end
+    def remove_blanks   attrs ; attrs.recursively_remove_by_value! { |v| (v != false) && v.blank? }         ; end
+    def styled_literal    str ; str.is_a?(String) ? StyledYAML.literal(str) : str                           ; end
+    def clean_attrs     attrs ; remove_blanks(attrs).recursively_replace_values! { |k,v| styled_literal v } ; end
 
     def build_association_table obj
       export_config(obj)[:associations].inject({ }) { |table, name|
