34130c2afe3b5ff8e5d11b6ff03d4d022e45d553
conanite <conan@conandalton.net>
2017-08-05 16:04:31 +0200

    exporter: simpler config setup

--- lib/protopack/exporter.rb
+++ lib/protopack/exporter.rb
@@ -5,14 +5,15 @@ module Protopack
     def array_assoc list     ; list.map { |item| to_attributes item }                                      ; end
     def clean_yaml hsh       ; StyledYAML.dump(clean_attrs hsh)                                            ; end
     def no_name? obj_id      ; raise "no name for object: #{obj_id.inspect}" if obj_id.blank?              ; end
-    def to_attributes obj    ; obj.slice(*obj.class.exportable_fields).merge build_association_table obj   ; end
+    def export_config    obj ; obj.protopack_export_config || { fields: [], associations: [] }             ; end
+    def to_attributes obj    ; obj.slice(*export_config(obj)[:fields]).merge build_association_table obj   ; end
     def to_yaml obj, meta={} ; clean_yaml to_package obj, meta                                             ; end
     def remove_blanks attrs  ; attrs.recursively_remove_by_value! { |v| (v != false) && v.blank? }         ; end
     def styled_literal str   ; str.is_a?(String) ? StyledYAML.literal(str) : str                           ; end
     def clean_attrs attrs    ; remove_blanks(attrs).recursively_replace_values! { |k,v| styled_literal v } ; end
 
     def build_association_table obj
-      obj.class.exportable_associations.inject({ }) { |table, name|
+      export_config(obj)[:associations].inject({ }) { |table, name|
         assoc = obj.send name
         next table if assoc.blank? || assoc.empty?
         attrs = (assoc.respond_to?(:each)) ? array_assoc(assoc) : to_attributes(assoc)
