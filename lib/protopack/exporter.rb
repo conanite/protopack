0ef8fa65609f6cb9336679f6d6f5ede73bf97f64
conanite <conan@conandalton.net>
2021-03-20 14:52:18 +0100

    exporter: use ruby YAML instead of StyledYAML to dump object

--- lib/protopack/exporter.rb
+++ lib/protopack/exporter.rb
@@ -3,7 +3,8 @@ module Protopack
     def maybe_name_methods    ; %i{ export_name full_name presentation_name name }                         ; end
     def name_method obj       ; maybe_name_methods.detect { |m| obj.respond_to?(m) && obj.send(m) }        ; end
     def array_assoc list      ; list.map { |item| to_attributes item }                                     ; end
-    def clean_yaml hsh        ; StyledYAML.dump(clean_attrs hsh)                                           ; end
+    # def clean_yaml hsh        ; StyledYAML.dump(clean_attrs hsh)                                           ; end
+    def clean_yaml hsh        ; YAML.dump(clean_attrs hsh)                                           ; end
     def no_name? obj_id       ; raise "no name for object: #{obj_id.inspect}" if obj_id.blank?             ; end
     def default_export_config ; { fields: [], associations: [] }                                           ; end
     def export_config     obj ; default_export_config.merge obj.protopack_export_config                    ; end
